d154833d870783b8a8d21290bf55ccaaa9c7dce5
hirakamu <achmadirsyadhamizan@gmail.com>
2025-11-06 11:36:23 +0700

    cleanup

--- ujian/scripts/cleanup.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST -H "Content-Type: application/json" localhost:5000/teacher/tokens/cleanup -d '{"force":"true"}'
-curl -X POST -H "Content-Type: application/json" localhost:5000/teacher/sessions/cleanup -d '{"force":"true"}'
